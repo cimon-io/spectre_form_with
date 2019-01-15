0603cf7614f3091344c012c449ad7e2fc8d31023
Alexey Osipenko <alexey@cimon.io>
2019-01-15 21:25:12 +0200

    stop using global variables

--- lib/spectre_form_with/helper.rb
+++ lib/spectre_form_with/helper.rb
@@ -1,17 +1,7 @@
 module SpectreFormWith
   module Helper
     def spectre_form_with(options = {}, &block)
-      temporarily_disable_field_error_proc do
-        form_with(options.merge({ builder: SpectreFormWith::FormBuilder }), &block)
-      end
-    end
-
-    def temporarily_disable_field_error_proc
-      original_proc = ActionView::Base.field_error_proc
-      ActionView::Base.field_error_proc = proc { |input, instance| input }
-      yield
-    ensure
-      ActionView::Base.field_error_proc = original_proc
+      form_with(options.merge({ builder: SpectreFormWith::FormBuilder }), &block)
     end
   end
 end
